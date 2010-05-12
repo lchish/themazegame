91b942f374ec9893bf821012c15d262d72b2b87b
xs42207 <xs42207@8c18819c-fffc-80f5-100b-b16e986ca6d0>
2010-05-12 01:21:30 +0000

    this time...

---
+++ trunk/TheMazeGame/TheMazeGame/render_world.h
@@ -0,0 +1,12 @@
+#ifndef RENDER_WORLD_HPP
+#define RENDER_WORLD_HPP
+
+
+extern void display();
+extern void setTempMaze(void);
+extern void reshape(int, int);
+extern void idle();
+void processNormalKeys(unsigned char key, int x, int y);
+void keyboardup(unsigned char key, int x, int y);
+
+#endif
