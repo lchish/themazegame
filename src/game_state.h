5619b45201623450583f88a54c8729a9c51b4ba6
lchish <lchish@8c18819c-fffc-80f5-100b-b16e986ca6d0>
2010-05-17 22:49:57 +0000

    We now have working game states hooray. When I get the time I will fix up the menu state so we can have some clickable buttons. By the way don't try to compile it as 64 bit yet.

--- src/game_state.h
+++ src/game_state.h
@@ -1,8 +1,14 @@
-#ifndef GAME_STATE_HPP
-#define GAME_STATE_HPP
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
 
 #define MENU_STATE_NUMBER 0
 #define GAME_STATE_NUMBER 1
+extern int CURRENT_STATE;
 
 void set_game_state(const int state_number);
+void gameStateUpdate();
+void gameStateReshape(int w,int h);
+void gameStateKeyboardFunc(unsigned char key, int x, int y);
+void gameStateKeyboardUpFunc(unsigned char key, int x, int y);
+void gameStateRender();
 #endif
