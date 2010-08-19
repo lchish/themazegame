0e3edd4b9df1e3e598558688d6341cfd35624d89
xs42207 <xs42207@8c18819c-fffc-80f5-100b-b16e986ca6d0>
2010-08-19 22:35:36 +0000

    Fixed up some small code. Mainly just added some #DEFINES, for the N_ROWS, N_COLUMNS, start and end x/y and now if you want to change the size all you have to do is go into defs.h and change N_ROWS and N_COLUMNS to what you want.

--- src/defs.h
+++ src/defs.h
@@ -1,8 +1,12 @@
-#ifndef DEFS_H_
-#define DEFS_H_
+#pragma once
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+
+#define WALL_TEX_COUNT 3
+#define FLOOR_TEX_COUNT 8
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,7 +15,7 @@ extern "C" {
 #define bool int
 
 #define true 1
-#define false 0;
+#define false 0
 
 #define NORTH 0
 #define EAST 1
@@ -19,8 +23,18 @@ extern "C" {
 #define WEST 3
 
 
+#define N_ROWS 8
+#define N_COLUMNS 3
+
+#define START_Y 0
+#define START_X 0
+
+#define END_X 99
+#define END_Y 99
+
+
 #ifdef __cplusplus
 }
 #endif 
 
-#endif
+
